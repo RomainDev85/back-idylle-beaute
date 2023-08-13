5bc988d080711d3de381ba4ad3fb6e4d92055f77
Romain Aubry <romain.aubry@espace-des-marques.com>
2023-08-13 17:27:50 +0200

    :hammer: Add crud for Service and category

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Service;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +16,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'app_admin_index')]
     public function index(): Response
     {
-        return parent::index();
+//        return parent::index();
+
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(CategoryCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -40,7 +48,11 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::linkToCrud('Category', 'fas fa-map-marker-alt', Category::class),
+            MenuItem::linkToCrud('Service', 'fas fa-map-marker-alt', Service::class),
+            MenuItem::linkToLogout('Logout', 'fa fa-exit'),
+        ];
     }
 }
