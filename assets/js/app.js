0f9c5cad67fd5dafd45d195856a457414fa0d98d
Romain Aubry <romain.aubry@espace-des-marques.com>
2023-12-15 22:34:28 +0100

    fix body scroll on menu open

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 const btnMenu = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu-navigation');
 const nav = document.querySelector('.navigation');
+const body = document.querySelector('body');
 
 const toggleMenu = () => {
+	body.classList.toggle('menu-open');
 	btnMenu.classList.toggle('open');
 	menu.classList.toggle('open');
 	nav.classList.toggle('open');
