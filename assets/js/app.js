713dea19cb2a248f03885390ab82bb3aff42f6d9
Romain Aubry <romain.aubry@espace-des-marques.com>
2023-12-15 22:26:36 +0100

    add menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,19 @@
-// const menu = new Menu();
-// menu.log();
+const btnMenu = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu-navigation');
+const nav = document.querySelector('.navigation');
 
-// window.menu.log('totoito')
+const toggleMenu = () => {
+	btnMenu.classList.toggle('open');
+	menu.classList.toggle('open');
+	nav.classList.toggle('open');
+}
+
+btnMenu.addEventListener('click', () => {
+	toggleMenu();
+})
+
+menu.addEventListener('click', (e) => {
+	if (e.target.tagName === "A") {
+		toggleMenu();
+	}
+})
