e61637b90616e08c1a4251863a7f24425ba8db80
Romain Aubry <romain.aubry@espace-des-marques.com>
2024-01-03 02:16:12 +0100

    add subcategory on categorypage

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\Front;
 
+use App\Entity\SubCategory;
 use App\Repository\CategoryRepository;
 use App\Repository\ServiceRepository;
+use App\Repository\SubCategoryRepository;
+use App\Repository\SubServiceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +18,8 @@ class CategoryController extends AbstractController
     public function __construct(
         private readonly ServiceRepository $serviceRepository,
         private readonly CategoryRepository $categoryRepository,
+        private readonly SubCategoryRepository $subCategoryRepository,
+        private readonly SubServiceRepository $subServiceRepository,
     )
     {
     }
@@ -30,11 +35,13 @@ class CategoryController extends AbstractController
             return $this->redirectToRoute('app_front_homepage');
         }
 
+        $subCategories = $this->subCategoryRepository->findBy(['category' => $category]);
         $services = $this->serviceRepository->findBy(['category' => $category]);
 
         return $this->render('category.html.twig', [
             'services' => $services,
             'category' => $category,
+            'subCategories' => $subCategories
         ]);
     }
 }
