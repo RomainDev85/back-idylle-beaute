38bc82ea20f1af86d9be03b2c64ea8bf19ff64ce
Romain Aubry <romain.aubry@espace-des-marques.com>
2024-01-20 23:36:10 +0100

    add in fixture all categoryImage

--- src/Fixture/Data/CategoryImageData.php
+++ src/Fixture/Data/CategoryImageData.php
@@ -27,6 +27,86 @@ class CategoryImageData
             "name" => "epilation5.jpg",
             "categoryName" => 'Epilation',
         ],
+        [
+            "name" => "autres-prestations1.jpg",
+            "categoryName" => 'Autres prestations',
+        ],
+        [
+            "name" => "autres-prestations2.jpg",
+            "categoryName" => 'Autres prestations',
+        ],
+        [
+            "name" => "autres-prestations3.jpg",
+            "categoryName" => 'Autres prestations',
+        ],
+        [
+            "name" => "autres-prestations4.jpg",
+            "categoryName" => 'Autres prestations',
+        ],
+        [
+            "name" => "autres-prestations5.jpg",
+            "categoryName" => 'Autres prestations',
+        ],
+        [
+            "name" => "manucure1.jpg",
+            "categoryName" => 'Manucure',
+        ],
+        [
+            "name" => "manucure2.jpg",
+            "categoryName" => 'Manucure',
+        ],
+        [
+            "name" => "manucure3.jpg",
+            "categoryName" => 'Manucure',
+        ],
+        [
+            "name" => "manucure4.jpg",
+            "categoryName" => 'Manucure',
+        ],
+        [
+            "name" => "manucure5.jpg",
+            "categoryName" => 'Manucure',
+        ],
+        [
+            "name" => "soins-du-corps1.jpg",
+            "categoryName" => 'Soins du corps',
+        ],
+        [
+            "name" => "soins-du-corps2.jpg",
+            "categoryName" => 'Soins du corps',
+        ],
+        [
+            "name" => "soins-du-corps3.jpg",
+            "categoryName" => 'Soins du corps',
+        ],
+        [
+            "name" => "soins-du-corps4.jpg",
+            "categoryName" => 'Soins du corps',
+        ],
+        [
+            "name" => "soins-du-corps5.jpg",
+            "categoryName" => 'Soins du corps',
+        ],
+        [
+            "name" => "soins-du-visage1.jpg",
+            "categoryName" => 'Soins du visage',
+        ],
+        [
+            "name" => "soins-du-visage2.jpg",
+            "categoryName" => 'Soins du visage',
+        ],
+        [
+            "name" => "soins-du-visage3.jpg",
+            "categoryName" => 'Soins du visage',
+        ],
+        [
+            "name" => "soins-du-visage4.jpg",
+            "categoryName" => 'Soins du visage',
+        ],
+        [
+            "name" => "soins-du-visage5.jpg",
+            "categoryName" => 'Soins du visage',
+        ],
     ];
 
     static function get(): array
